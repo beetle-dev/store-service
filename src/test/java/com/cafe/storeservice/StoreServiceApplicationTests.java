71c37ae938f0b54d9841f19ae9aeef290839e254
JangSuhyeon <shjang0043@gmail.com>
2026-05-26 16:21:49 +0900

    chore : Dockerfile 추가

--- src/test/java/com/cafe/storeservice/StoreServiceApplicationTests.java
+++ src/test/java/com/cafe/storeservice/StoreServiceApplicationTests.java
@@ -2,8 +2,12 @@ package com.cafe.storeservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.ReactiveStringRedisTemplate;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class StoreServiceApplicationTests {
 
     @Test
