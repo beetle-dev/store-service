6acf15eb70feb0d5cc74896ee96c087435ddcae8
JangSuhyeon <shjang0043@gmail.com>
2026-05-29 17:49:12 +0900

    feat: 신규 주문 자동 생성 스케줄러 추가

--- src/main/java/com/cafe/storeservice/repository/MenuRepository.java
+++ src/main/java/com/cafe/storeservice/repository/MenuRepository.java
@@ -14,5 +14,5 @@ public interface MenuRepository extends JpaRepository<Menu, Long> {
 
     Optional<Menu> findByName(String name);
 
-    List<Menu> findByStoreIdAndActiveTrue(Long id);
+    List<Menu> findByStoreIdAndIsActiveTrue(Long id);
 }
