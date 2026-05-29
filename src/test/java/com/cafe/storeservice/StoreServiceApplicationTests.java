d474cc173e7b4eb975fe79d21e755f33e1a08f7f
JangSuhyeon <shjang0043@gmail.com>
2026-05-29 15:17:12 +0900

    chore: StringRedisTemplate MockBean 추가

--- src/test/java/com/cafe/storeservice/StoreServiceApplicationTests.java
+++ src/test/java/com/cafe/storeservice/StoreServiceApplicationTests.java
@@ -3,12 +3,16 @@ package com.cafe.storeservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.ReactiveStringRedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class StoreServiceApplicationTests {
+
+    @MockitoBean
+    StringRedisTemplate redisTemplate;
 
     @Test
     void contextLoads() {
